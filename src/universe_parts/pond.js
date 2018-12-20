e5792e592c1436b07a14b367a1f4f15e7a121f2e
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-12-20 20:05:55 +0530

    Added pond

---
+++ src/universe_parts/pond.js
@@ -0,0 +1,61 @@
+
+defineThreeUniverse(function (THREE, UNIVERSE, options) {
+    var queryTexture = null;
+    return new Promise(function (resolve, reject) {
+
+        var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 1, 1);
+
+
+
+            // var groundTexture = textures[0];
+            // groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+            // groundTexture.repeat.set(25, 25);
+            // groundTexture.anisotropy = 16;
+
+            // var queryTexture= textures[1];
+            // var displacementMap = queryTexture.texture;
+
+
+            var material = new THREE.MeshBasicMaterial({
+                color:'blue' 
+            });
+
+            var mesh = new THREE.Mesh(geometry, material);
+            mesh.rotation.x = - Math.PI / 2;
+            mesh.position.y= -40;
+            mesh.receiveShadow = true;
+
+            
+
+            // var originalRaycast = mesh.raycast;
+            // mesh.raycast = function (raycaster, intersects) {
+            //     originalRaycast.call(mesh, raycaster, intersects);
+            //     var thisobjectsIntersects = intersects.filter((element) => element.object == mesh);
+            //     if (thisobjectsIntersects.length) {
+            //         let uv = new THREE.Vector2().copy(thisobjectsIntersects[0].uv);
+            //         displacementMap.transformUv(uv);
+            //         var hightpixel = queryTexture.getPixelAtUv(uv.x, uv.y);
+            //         var hightVal = hightpixel.r / 255 * material.displacementScale + material.displacementBias;
+            //     }
+
+            //     thisobjectsIntersects.forEach(element => {
+            //         element.point.y = hightVal;
+
+            //     });
+
+
+
+            // }
+
+            // UNIVERSE.GroundManager.add(mesh);
+            resolve(mesh);
+
+        });
+
+
+
+
+
+
+
+});
