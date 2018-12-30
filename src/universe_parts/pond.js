79b62bcbe04cb9a12f8c6fdab36d140009d744b5
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-12-30 10:15:41 +0530

    Added pond shader

--- src/universe_parts/pond.js
+++ src/universe_parts/pond.js
@@ -5,7 +5,7 @@ defineThreeUniverse(function (THREE, UNIVERSE, options) {
 
         var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 1, 1);
 
-
+        options.requestAnimationFrame(()=>{});
 
             // var groundTexture = textures[0];
             // groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
@@ -24,11 +24,39 @@ defineThreeUniverse(function (THREE, UNIVERSE, options) {
             // } );
             
             
-            var material = new THREE.MeshLambertMaterial({
-                color:'#4285F4', 
-                transparent:true,
-                opacity :0.9
-            });
+            // var material = new THREE.MeshLambertMaterial({
+            //     color:'#4285F4', 
+            //     transparent:true,
+            //     opacity :0.9
+            // });
+
+            let code = `void mainImage( out vec4 fragColor, in vec2 fragCoord )
+            {
+                // input: pixel coordinates
+                vec2 p = (-iResolution.xy + 2.0*fragCoord)/iResolution.y;
+            
+                // angle of each pixel to the center of the screen
+                float a = atan(p.y,p.x);
+                
+                // modified distance metric
+                float r = pow( pow(p.x*p.x,4.0) + pow(p.y*p.y,4.0), 1.0/8.0 );
+                
+                // index texture by (animated inverse) radious and angle
+                vec2 uv = vec2( 1.0/r + 0.2*iTime, a );
+            
+                // pattern: cosines
+                float f = cos(12.0*uv.x)*cos(6.0*uv.y);
+            
+                // color fetch: palette
+                vec3 col = 0.5 + 0.5*sin( 3.1416*f + vec3(0.0,0.5,1.0) );
+                
+                // lighting: darken at the center    
+                col = col*r;
+                
+                // output: pixel color
+                fragColor = vec4( col, 1.0 );
+            }`
+            var material = new THREE.ShaderToyMaterial(code)
 
             var mesh = new THREE.Mesh(geometry, material);
             mesh.rotation.x = - Math.PI / 2;
