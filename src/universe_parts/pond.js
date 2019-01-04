088c9b8dcd0862ec76eebe3e47e32599a0a117b4
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2019-01-04 09:53:46 +0530

    Updated shader for pond

--- src/universe_parts/pond.js
+++ src/universe_parts/pond.js
@@ -30,31 +30,59 @@ defineThreeUniverse(function (THREE, UNIVERSE, options) {
             //     opacity :0.9
             // });
 
-            let code = `void mainImage( out vec4 fragColor, in vec2 fragCoord )
+            let code = `// Found this on GLSL sandbox. I really liked it, changed a few things and made it tileable.
+            // :)
+            // by David Hoskins.
+            
+            
+            // Water turbulence effect by joltz0r 2013-07-04, improved 2013-07-07
+            
+            
+            // Redefine below to see the tiling...
+            // #define SHOW_TILING
+            
+            #define TAU 6.28318530718
+            #define MAX_ITER 5
+            
+            void mainImage( out vec4 fragColor, in vec2 fragCoord ) 
             {
-                // input: pixel coordinates
-                vec2 p = (-iResolution.xy + 2.0*fragCoord)/iResolution.y;
+                float time = iTime * .1+23.0;
+                // uv should be the 0-1 uv of texture...
+                vec2 uv = fragCoord.xy / iResolution.xy;
+                
+            #ifdef SHOW_TILING
+                vec2 p = mod(uv*TAU*2.0, TAU)-250.0;
+            #else
+                vec2 p = mod(uv*TAU, TAU)-250.0;
+            #endif
+                vec2 i = vec2(p);
+                float c = 1.0;
+                float inten = .005;
             
-                // angle of each pixel to the center of the screen
-                float a = atan(p.y,p.x);
+                for (int n = 0; n < MAX_ITER; n++) 
+                {
+                    float t = time * (1.0 - (3.5 / float(n+1)));
+                    i = p + vec2(cos(t - i.x) + sin(t + i.y), sin(t - i.y) + cos(t + i.x));
+                    c += 1.0/length(vec2(p.x / (sin(i.x+t)/inten),p.y / (cos(i.y+t)/inten)));
+                }
+                c /= float(MAX_ITER);
+                c = 1.17-pow(c, 1.4);
+                vec3 colour = vec3(pow(abs(c), 8.0));
+                colour = clamp(colour + vec3(0.0, 0.35, 0.5), 0.0, 1.0);
                 
-                // modified distance metric
-                float r = pow( pow(p.x*p.x,4.0) + pow(p.y*p.y,4.0), 1.0/8.0 );
-                
-                // index texture by (animated inverse) radious and angle
-                vec2 uv = vec2( 1.0/r + 0.2*iTime, a );
             
-                // pattern: cosines
-                float f = cos(12.0*uv.x)*cos(6.0*uv.y);
+                #ifdef SHOW_TILING
+                // Flash tile borders...
+                vec2 pixel = 2.0 / iResolution.xy;
+                uv *= 2.0;
             
-                // color fetch: palette
-                vec3 col = 0.5 + 0.5*sin( 3.1416*f + vec3(0.0,0.5,1.0) );
+                float f = floor(mod(iTime*.5, 2.0)); 	// Flash value.
+                vec2 first = step(pixel, uv) * f;		   	// Rule out first screen pixels and flash.
+                uv  = step(fract(uv), pixel);				// Add one line of pixels per tile.
+                colour = mix(colour, vec3(1.0, 1.0, 0.0), (uv.x + uv.y) * first.x * first.y); // Yellow line
                 
-                // lighting: darken at the center    
-                col = col*r;
-                
-                // output: pixel color
-                fragColor = vec4( col, 1.0 );
+                #endif
+                fragColor = vec4(colour, 0.7);
             }`
             var material = new THREE.ShaderToyMaterial(code)
 
